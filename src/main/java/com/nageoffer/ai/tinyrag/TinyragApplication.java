f08f4b3fc39c86e57a3ac128fc05bc48560ac6a0
ding.ma <machen@apache.org>
2026-02-10 19:38:35 +0800

    feature(init): 初始化TinyRAG项目结构和文件

---
+++ src/main/java/com/nageoffer/ai/tinyrag/TinyragApplication.java
@@ -0,0 +1,12 @@
+package com.nageoffer.ai.tinyrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyragApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyragApplication.class, args);
+    }
+}
