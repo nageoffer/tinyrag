dc0c66f1878edb88060601a4e89a93781175f898
ding.ma <machen@apache.org>
2026-03-15 11:13:28 +0800

    refactor(server): 重命名主应用程序类以简化名称

--- tinyrag-mcp-server/src/main/java/com/nageoffer/ai/tinyrag/mcp/McpServerApplication.java
+++ tinyrag-mcp-server/src/main/java/com/nageoffer/ai/tinyrag/mcp/McpServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinyragMcpServerApplication {
+public class McpServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TinyragMcpServerApplication.class, args);
+        SpringApplication.run(McpServerApplication.class, args);
     }
 }
